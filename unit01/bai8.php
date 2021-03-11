146956ec1647d12ba4d3a1b09a7f807e7d765dc2
xuanthuc <xuanthuc2x@gmail.com>
2021-03-11 16:27:42 +0700

    bài tập PHP

--- unit01/bai8.php
+++ unit01/bai8.php
@@ -7,12 +7,16 @@
 </head>
 <body>
 	<?php
-		for($i = 5; $i >= 1; $i --) {
-	    for($j = 0; $j < $i; $j ++) {
+		for($i = 1; $i <=5; $i ++) {
+	    for($j = 1; $j <=5; $j ++) {
+	    	if ($i <= $j) {
 	        echo "#";
+	    	} else {
+	    		echo "&nbsp;&nbsp;";
+	    	}
 	    }
 	   		echo "<br>";
 		}
 	?>
 </body>
-</html>
+</html>		
