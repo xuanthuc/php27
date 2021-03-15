2c9789d339face8cb3f7f684e3086c80018053a1
xuanthuc <xuanthuc2x@gmail.com>
2021-03-15 13:56:17 +0700

    zent27.php

---
+++ unit02/bai 2.7/users.php
@@ -0,0 +1,13 @@
+<?php 
+	require_once('header.php')
+ ?>
+	<div class="container-fluid">
+
+		<!-- Page Heading -->
+		<h1 class="h3 mb-4 text-gray-800">Đây là trang quản lý người dùng</h1>
+
+	</div>
+
+ <?php 
+	require_once('footer.php')
+ ?>
